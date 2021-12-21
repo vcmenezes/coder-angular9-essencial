016f17f1ce1c640ded119e280443383ec9779d68
Vinicius Menezes <vicmen22@gmail.com>
2021-12-21 16:33:19 -0400

    Método Excluir Produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'products', component: ProductCrudComponent},
   {path: 'products/create', component: ProductCreateComponent},
-  {path: 'products/update/:id', component: ProductUpdateComponent}
+  {path: 'products/update/:id', component: ProductUpdateComponent},
+  {path: 'products/delete/:id', component: ProductUpdateComponent}
 ];
 
 @NgModule({
